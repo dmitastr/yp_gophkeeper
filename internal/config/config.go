fd8866096976ebf759cf129a796f81e318eff791
Dmitrii <kalla.mallanik@gmail.com>
2025-12-24 23:26:21 +0300

    limit incoming requests

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,16 +24,20 @@ type Config struct {
 	Address   string `json:"address" env:"ADDRESS" mapstructure:"address"`
 	DBConnStr string `json:"db_conn_str" env:"DBCONNSTR" mapstructure:"db-conn-str"`
 	Key       string `json:"key" env:"KEY" mapstructure:"key"`
+	MaxSize   int64  `json:"max_size" env:"MAX_SIZE" mapstructure:"max-size"`
 }
 
 func NewConfig() (ConfigProvider, error) {
 	log := logger.NewLogger()
 	cp := configProvider{logger: log}
 
+	maxSizeDefault := int64(100) << 20
+
 	flagSet := pflag.NewFlagSet("GophKeeper", pflag.ExitOnError)
 	flagSet.StringP("address", "a", "localhost:8080", "set app host and port")
 	flagSet.StringP("db-conn-str", "d", "", "postgres connection url")
 	flagSet.StringP("key", "k", "", "key for encrypting data")
+	flagSet.Int64P("max-size", "m", maxSizeDefault, "key for encrypting data")
 
 	if err := flagSet.Parse(os.Args[1:]); err != nil {
 		return nil, fmt.Errorf("error parsing flags: %w", err)
@@ -47,6 +51,7 @@ func NewConfig() (ConfigProvider, error) {
 	_ = viper.BindEnv("address", "ADDRESS")
 	_ = viper.BindEnv("db-conn-str", "DBCONNSTR")
 	_ = viper.BindEnv("key", "KEY")
+	_ = viper.BindEnv("max-size", "MAX_SIZE")
 
 	if cfgPath := viper.GetString("configProvider"); cfgPath != "" {
 		viper.SetConfigFile(cfgPath)
--- internal/presentation/middleware/middleware.go
+++ internal/presentation/middleware/middleware.go
@@ -7,11 +7,16 @@ import (
 
 type MiddlewareProvider interface {
 	BearerValidator
+	SizeChecker
 }
 type MiddlewareProviderImpl struct {
 	BearerValidator
+	SizeChecker
 }
 
 func NewMiddlewareProvider(cfg config.ConfigProvider, serviceProvider service.IService) MiddlewareProvider {
-	return &MiddlewareProviderImpl{BearerValidator: NewBearerValidator(cfg, serviceProvider)}
+	return &MiddlewareProviderImpl{
+		BearerValidator: NewBearerValidator(cfg, serviceProvider),
+		SizeChecker:     NewSizeChecker(cfg),
+	}
 }
